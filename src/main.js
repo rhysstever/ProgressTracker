93ac9d0e46de4daba03cbeb7244ad0c5ac27a967
Rhys Stever <rhysstever3@gmail.com>
2025-08-31 16:27:29 -0400

    Initial project setup

---
+++ src/main.js
@@ -0,0 +1,29 @@
+let fps = 20;
+let prevLarge = true;
+
+const init = () => {
+  // Loop if needed
+//   loop();
+  setup()
+}
+
+const loop = () => {
+  // If the window was large and is now 425px or smaller
+  if (prevLarge && window.innerWidth <= 425) {
+    prevLarge = false;
+    // Rerender something 
+  } else if (!prevLarge && window.innerWidth > 425) {
+    prevLarge = true;
+    // Rerender something 
+  }
+  // Call loop() again
+  requestAnimationFrame(loop, 1000 / fps);
+}
+
+const setup = () => {
+  
+}
+
+export { 
+  init
+};
